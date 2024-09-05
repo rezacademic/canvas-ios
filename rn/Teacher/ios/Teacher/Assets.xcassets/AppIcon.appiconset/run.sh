cba2977feaffc1a81b192718fba76fb650acd201
Shadow <hi@shadowing.dev>
2024-09-05 12:48:05 -0500

    icons woo

--- rn/Teacher/ios/Teacher/Assets.xcassets/AppIcon.appiconset/run.sh
+++ rn/Teacher/ios/Teacher/Assets.xcassets/AppIcon.appiconset/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Declare an array
-arr=("20x20", "29x29", "40x40", "58x58", "60x60", "76x76", "80x80", "87x87", "120x120", "152x152", "167x167", "180x180")
+arr=("20x20" "29x29" "40x40" "58x58" "60x60" "76x76" "80x80" "87x87" "120x120" "152x152" "167x167" "180x180")
 
 # Loop through each element
 for element in "${arr[@]}"; do
